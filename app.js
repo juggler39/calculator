347d80243f2759c8013ef87feac77d6cd73c4220
juggler39 <juggler39@yandex.ru>
2020-07-23 00:02:46 +0200

    make pwa

--- app.js
+++ app.js
@@ -28,3 +28,13 @@ function parseKey(e) {
         button.click();
     }
 }
+
+if (navigator.serviceWorker.controller) {
+    console.log('[PWA Builder] active service worker found, no need to register')
+  } else {
+    navigator.serviceWorker.register('sw.js', {
+      scope: './'
+    }).then(function(reg) {
+      console.log('Service worker has been registered for scope:'+ reg.scope);
+    });
+  }
