bf634f890416789575ccd8b82320883dbebc2ff3
juggler39 <juggler39@yandex.ru>
2020-07-23 23:20:59 +0200

    ready state

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 'use strict';
 const buttons = document.querySelectorAll('.button');
 const screen = document.querySelector('.screen');
+const memory = document.querySelector('.memory');
+const error = document.querySelector('.error');
 
 buttons.forEach(button => button.addEventListener('click', function(e) {
     if (button.classList.contains('number')) {
         c.inputDigit(button.id);
-    } else if (button.classList.contains('separator')) {
-        c.inputPoint(button.id);
     } else if (button.classList.contains('operator')) {
         c.inputOperator(button.id);
     } else (c.inputAction(button.id));
     
-    screen.innerHTML = c.updateDisplay();
+    if (!c.error) {
+      screen.innerHTML = c.updateDisplay();
+    }
+    memory.innerHTML = c.memory ? 'M' : '';
+    error.innerHTML = c.error ? 'E' : '';
 }));
 
 window.addEventListener('keydown', parseKey);
@@ -30,11 +34,11 @@ function parseKey(e) {
 }
 
 if (navigator.serviceWorker.controller) {
-    console.log('[PWA Builder] active service worker found, no need to register')
+    //console.log('[PWA Builder] active service worker found, no need to register')
   } else {
     navigator.serviceWorker.register('sw.js', {
       scope: './'
     }).then(function(reg) {
-      console.log('Service worker has been registered for scope:'+ reg.scope);
+      //console.log('Service worker has been registered for scope:'+ reg.scope);
     });
   }
--- calculator.js
+++ calculator.js
@@ -6,45 +6,15 @@ c.operator = '';
 c.firstNumber = '0';
 c.editable = true;
 c.maxLength = 8;
+c.memory = 0;
+c.error = false;
+c.previousKey = '';
+c.previousNumber = 0;
 
 c.add = (a, b) => a + b;
 c.substract = (a, b) => a - b;
 c.multiply = (a, b) => a * b;
 c.divide = (a, b) => a / b;
-
-c.inputDigit = (digit) => {
-    if (!c.editable) {
-        c.editable = true;
-        c.input = '0';
-    }
-    if (c.input.replace(/\./, '').length < c.maxLength) {
-        if (c.input === '0') {
-            if (digit !== '0') {
-                c.input = digit;
-            }
-        } else {
-            c.input += digit;
-        }
-        c.screen = c.input;
-    }
-}
-
-c.inputPoint = () => {
-    c.editable = true;
-    if (c.input ==='0') {
-        c.input = '0.';
-    } else if (c.input.indexOf('.') === -1) {
-        c.input += '.';
-    }
-    c.screen = c.input;
-}
-
-c.inputOperator = (operator) => {
-    if (c.firstNumber != '0') c.equals();
-    c.operator = operator;
-    c.firstNumber = c.input;
-    c.input = '0';
-}
 
 c.operate =  (a, b, op) => {
     switch (op) {
@@ -52,7 +22,6 @@ c.operate =  (a, b, op) => {
         case '-': return c.substract (a,b);
         case '*': return c.multiply (a, b);
         case '/': return c.divide (a, b);
-        default: return 0;
     }
 }
 
@@ -63,38 +32,138 @@ c.equals = () => {
     c.editable = false;
 }
 
-c.inputAction = (action) => {
-    switch(action) {
-        case 'Enter':
-            c.equals();
-            break;
-        case '%':
-            c.input = +c.input*c.firstNumber/100;
-            c.equals();
-            break;
-        case '+/-': 
-            c.input = '' + (0 - c.input);
-            c.screen = c.input;
-            break;
-        case 'Backspace':
-            if (c.editable) c.input = c.input.slice(0, -1);
-            c.screen = c.input;
-            break;
-    }
-}
-
 c.updateDisplay = () => {
     if (c.editable) {
-
         if (c.screen.toString().indexOf('.') === -1) {
             return c.screen + '.';
         } else {
             return c.screen;
         }
-
     } else {
-        c.screen = '' + Number(c.screen).toPrecision(c.maxLength);
-        return c.screen.replace(/(\.\d*?)0+$/, "$1");
-
+        if (Math.abs(c.screen) >= 10 ** c.maxLength) {
+            c.error = true;
+            c.screen = c.screen/(10 ** c.maxLength);
+            if (Math.abs(c.screen) === Infinity) c.screen = 0;
+            c.input = c.screen;
+            return c.screen.toPrecision(c.maxLength);
+        } else {
+            c.screen = Number(c.screen).toPrecision(c.maxLength);
+            if (Math.abs(c.screen) < 1) {
+                c.screen = '' + Number(c.screen).toFixed(c.maxLength-1);
+            }
+            return c.screen.toString().replace(/(\.\d*?)0+$/, "$1");
+        }
     }
+}
+
+c.inputDigit = (digit) => {
+    if (!c.editable) {
+        c.editable = true;
+        c.input = '0';
+    }
+    if (c.input.replace(/[.-]/g, '').length < c.maxLength) {
+        if (c.input.replace(/-/, '') === '0') {
+            if (digit !== '0') {
+                if (c.input[0] === '-') {
+                    c.input = '-' + digit;
+                } else {
+                    c.input = digit;
+                }
+            }
+        } else {
+            c.input += digit;
+        }
+        c.screen = c.input;
+    }
+    c.previousKey = digit;
+}
+
+c.inputPoint = () => {
+    if (!c.editable) {
+        c.editable = true;
+        c.input = '0';
+    }
+    if (c.input ==='0') {
+        c.input = '0.';
+    } else if (c.input.indexOf('.') === -1) {
+        c.input += '.';
+    }
+}
+
+c.inputOperator = (operator) => {
+    
+    if (c.previousKey === 'operator') {
+        c.operator = operator;
+        c.firstNumber = c.previousNumber;
+        c.input = '0';
+    } else {
+        if (c.firstNumber != '0') c.equals();
+        c.operator = operator;
+        c.previousNumber = c.input;
+        c.firstNumber = c.input;
+        c.input = '0';
+    }
+    c.previousKey = 'operator';
+}
+
+c.inputAction = (action) => {
+    switch(action) {
+        case 'Enter':
+            if (c.previousKey === 'Enter') c.firstNumber = c.previousNumber;
+            c.equals();
+            break;
+        case '%':
+            c.editable = false;
+            if (c.firstNumber != '0') {
+                c.input = +c.input * c.firstNumber / 100;
+            } else {
+                c.input = +c.input / 100;
+            }
+            c.screen = c.input;
+            break;
+        case '+/-':
+            c.input = c.input[0]==='-'?c.input.slice(1):'-' + c.input;
+            c.screen = c.input;
+            break;
+        case 'Backspace':
+            if (c.editable) c.input = c.input.slice(0, -1)||'0';
+            c.screen = c.input;
+            break;
+        case '.':
+            c.inputPoint();
+            c.screen = c.input;
+            break;
+        case 'M+':
+            c.editable = false;
+            if (c.firstNumber != '0') c.equals();
+            c.memory = c.memory + c.input;
+            break;
+        case 'M-':
+            c.editable = false;
+            if (c.firstNumber != '0') c.equals();
+            c.memory = c.memory - c.input;
+            break;
+        case 'MRC':
+            if (c.previous === 'MRC') {
+                c.memory = 0;
+            } else {
+                c.editable = false;
+                c.input = c.memory;
+                c.screen = c.input;
+            }
+            break;
+        case 'CE':
+            c.error = false;
+            c.input = '0';
+            c.screen = c.input;
+            break;
+        case 'ON/C':
+            c.error = false;
+            c.memory = 0;
+            c.firstNumber = '0';
+            c.input = '0';
+            c.screen = c.input;
+            break;
+    }
+    c.previousKey = action;
 }
